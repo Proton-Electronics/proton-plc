f542620d8d0e4cc142794c1486f7fbb61d2c2502
Hamza BENDALI BRAHAM <hbendali@ya.ru>
2022-11-18 15:42:41 +0100

    up to 0.0.18a

--- Proton_Arduino_PLC/libraries/A5/src/A5.cpp
+++ Proton_Arduino_PLC/libraries/A5/src/A5.cpp
@@ -15,36 +15,119 @@
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- 
+
   Modified 2012 by Todd Krein (todd@krein.org) to implement repeated starts
   Modified 2017 by Chuck Todd (ctodd@cableone.net) to correct Unconfigured Slave Mode reboot
   Modified 2020 by Greyson Christoforo (grey@christoforo.net) to implement timeouts
 */
 
 #include "A5.h"
+//#include "Arduino.h”
 
-Proton_Arduino_PLC::Proton_Arduino_PLC() {
-    pinMode(D1, INPUT);
-    pinMode(D2, INPUT);
-    pinMode(D3, INPUT);
-    pinMode(D4, INPUT);
-    pinMode(D5, INPUT);
-    pinMode(D6, INPUT);
+Proton_Arduino_PLC::Proton_Arduino_PLC()
+{
 
-    pinMode(A1, INPUT);
-    pinMode(A2, INPUT);
-    pinMode(A3, INPUT);
-    pinMode(A4, INPUT);
+  pinMode(D1, INPUT);
+  pinMode(D2, INPUT);
+  pinMode(D3, INPUT);
+  pinMode(D4, INPUT);
+  pinMode(D5, INPUT);
+  pinMode(D6, INPUT);
 
-    pinMode(AO1, OUTPUT);
-    pinMode(AO2, OUTPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A4, INPUT);
 
-    pinMode(REL1, OUTPUT);
-    pinMode(REL2, OUTPUT);
-    pinMode(REL3, OUTPUT);
-    pinMode(REL4, OUTPUT);
-    pinMode(REL5, OUTPUT);
+  pinMode(AO1, OUTPUT);
+  pinMode(AO2, OUTPUT);
 
-    Serial.begin(115200);
-    Wire.begin();
+  pinMode(REL1, OUTPUT);
+  pinMode(REL2, OUTPUT);
+  pinMode(REL3, OUTPUT);
+  pinMode(REL4, OUTPUT);
+  pinMode(REL5, OUTPUT);
+
+  Serial.begin(115200);
+}
+
+int Proton_Arduino_PLC::d1(void)
+{
+  return digitalRead(D1);
+}
+int Proton_Arduino_PLC::d2(void)
+{
+  return digitalRead(D2);
+}
+int Proton_Arduino_PLC::d3(void)
+{
+  return digitalRead(D3);
+}
+int Proton_Arduino_PLC::d4(void)
+{
+  return digitalRead(D4);
+}
+int Proton_Arduino_PLC::d5(void)
+{
+  return digitalRead(D5);
+}
+int Proton_Arduino_PLC::d6(void)
+{
+  return digitalRead(D6);
+}
+int Proton_Arduino_PLC::a1(void)
+{
+  return analogRead(A1);
+}
+int Proton_Arduino_PLC::a2(void)
+{
+  return analogRead(A2);
+}
+int Proton_Arduino_PLC::a3(void)
+{
+  return analogRead(A3);
+}
+int Proton_Arduino_PLC::a4(void)
+{
+  return analogRead(A4);
+}
+
+int Proton_Arduino_PLC::read(int D1_D6_A1_A4)
+{
+  if (D1_D6_A1_A4 == D1 || D1_D6_A1_A4 == D2 || D1_D6_A1_A4 == D3 || D1_D6_A1_A4 == D4 || D1_D6_A1_A4 == D5 || D1_D6_A1_A4 == D6)
+    return digitalRead(D1_D6_A1_A4);
+  else if (D1_D6_A1_A4 == A1 || D1_D6_A1_A4 == A2 || D1_D6_A1_A4 == A3 || D1_D6_A1_A4 == A4)
+    return analogRead(D1_D6_A1_A4);
+  else
+    return false;
+}
+
+void Proton_Arduino_PLC::rel1(int ON_OFF)
+{
+  digitalWrite(REL1, ON_OFF);
+}
+void Proton_Arduino_PLC::rel2(int ON_OFF)
+{
+  digitalWrite(REL2, ON_OFF);
+}
+void Proton_Arduino_PLC::rel3(int ON_OFF)
+{
+  digitalWrite(REL3, ON_OFF);
+}
+void Proton_Arduino_PLC::rel4(int ON_OFF)
+{
+  digitalWrite(REL4, ON_OFF);
+}
+void Proton_Arduino_PLC::rel5(int ON_OFF)
+{
+  digitalWrite(REL5, ON_OFF);
+}
+
+void Proton_Arduino_PLC::on(int REL1_REL5)
+{
+  digitalWrite(REL1_REL5, HIGH);
+}
+void Proton_Arduino_PLC::off(int REL1_REL5)
+{
+  digitalWrite(REL1_REL5, LOW);
 }
--- Proton_Arduino_PLC/libraries/A5/src/A5.h
+++ Proton_Arduino_PLC/libraries/A5/src/A5.h
@@ -1,9 +1,36 @@
 #ifndef _VARIANT_A5_
 #define _VARIANT_A5_
 
-class Proton_Arduino_PLC {
+#include "Arduino.h"
+
+class Proton_Arduino_PLC
+{
 public:
 	Proton_Arduino_PLC();
+
+	int d1(void);
+	int d2(void);
+	int d3(void);
+	int d4(void);
+	int d5(void);
+	int d6(void);
+
+	int a1(void);
+	int a2(void);
+	int a3(void);
+	int a4(void);
+
+	int read(int D1_D6_A1_A4);
+
+	void rel1(int ON_OFF);
+	void rel2(int ON_OFF);
+	void rel3(int ON_OFF);
+	void rel4(int ON_OFF);
+	void rel5(int ON_OFF);
+
+	void on(int REL1_REL5);
+	void off(int REL1_REL5);
+
 private:
 };
 
